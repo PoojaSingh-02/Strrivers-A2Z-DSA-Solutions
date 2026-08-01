dcec7024f2193d1bb26c95117cf1d416ef65f809
POOJA SINGH <poojaasingh.0212@gmail.com>
2026-08-01 13:53:53 +0530

    Create While_Loop.cpp

---
+++ While_Loop.cpp
@@ -0,0 +1,16 @@
+ class Solution {
+    public:
+    int whileLoop(int d) {
+        int sum=0;
+        int count=0;
+        int num=1;
+         while(count<50) {
+            if(num%10==d) {
+              sum=sum+num;
+              count++;  
+            }
+            num++;
+         }
+         return sum;  
+    }
+};
